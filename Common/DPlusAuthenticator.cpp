3107a7f60edb220a589612d118d7bf4213622f1d
Jonathan Naylor <naylorjs@yahoo.com>
2018-09-23 15:42:00 +0100

    Update the D-Plus authenticator host.

--- Common/DPlusAuthenticator.cpp
+++ Common/DPlusAuthenticator.cpp
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) 2010-2015 by Jonathan Naylor G4KLX
+ *   Copyright (C) 2010-2015,2018 by Jonathan Naylor G4KLX
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -22,7 +22,7 @@
 #include "Utils.h"
 #include "Defs.h"
 
-const wxString OPENDSTAR_HOSTNAME = wxT("opendstar.org");
+const wxString OPENDSTAR_HOSTNAME = wxT("auth.dstargateway.org");
 const unsigned int OPENDSTAR_PORT = 20001U;
 
 const wxString DUTCHSTAR_HOSTNAME = wxT("dpns.dutch-star.eu");
