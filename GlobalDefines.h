dcb1a8a5660ef170f4b79b6491acab961572712b
Shawn Chain <shawn.chain@gmail.com>
2018-11-09 13:40:06 +0800

    Update DStarGateway Global Defs

--- GlobalDefines.h
+++ GlobalDefines.h
@@ -6,21 +6,18 @@
 #define LOG_DIR  "/opt/mmdvm/logs"
 #define CONF_DIR "/opt/mmdvm/conf"
 #define CONF_FILE "ircDDBGateway.ini"
-#define LOG_BASE "ircDDBGateway"
 
 #elif defined(OPENWRT) && OPENWRT == 1
 #define DATA_DIR "/etc/mmdvm/dstar"
 #define LOG_DIR  "/var/log/mmdvm"
 #define CONF_DIR "/etc"
 #define CONF_FILE "ircDDBGateway.ini"
-#define LOG_BASE "ircDDBGateway"
 
 #else
 #define DATA_DIR "/usr/share/ircddbgateway"
 #define LOG_DIR  "/tmp"
 #define CONF_DIR "/etc"
 #define CONF_FILE "ircddbgateway"
-#define LOG_BASE "ircddbgatewayd"
 
 #endif
 
