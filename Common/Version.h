b8ee3948ed09b4f1f6941e1d918e797b98387068
Jonathan Naylor <jonathan.naylor@igence.com>
2019-01-31 08:28:36 +0000

    Bump the version date.

--- Common/Version.h
+++ Common/Version.h
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) 2010-2015,2018 by Jonathan Naylor G4KLX
+ *   Copyright (C) 2010-2015,2018,2019 by Jonathan Naylor G4KLX
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -24,9 +24,9 @@
 const wxString VENDOR_NAME = wxT("G4KLX");
 
 #if defined(__WXDEBUG__)
-const wxString VERSION = wxT("20180719 - DEBUG");
+const wxString VERSION = wxT("20190131 - DEBUG");
 #else
-const wxString VERSION = wxT("20180719");
+const wxString VERSION = wxT("20190131");
 #endif
 
 #endif
