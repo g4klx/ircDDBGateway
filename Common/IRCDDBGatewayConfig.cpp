746430b9bff65f79c456e0d7db3837880fbce107
Geoffrey Merck F4FXL - KC3FRA <F4FXL@users.noreply.github.com>
2019-03-26 11:07:57 +0100

    Fix broken windows build

--- Common/IRCDDBGatewayConfig.cpp
+++ Common/IRCDDBGatewayConfig.cpp
@@ -760,8 +760,8 @@ m_y(DEFAULT_WINDOW_Y)
 	m_config->Read(m_name + KEY_XLX_ENABLED, &m_xlxEnabled, DEFAULT_XLX_ENABLED);
 		
 	m_config->Read(m_name + KEY_XLX_HOSTS_FILE_URL, &m_xlxHostsFileUrl, DEFAULT_XLX_HOSTS_FILE_URL);
-	if(m_xlxEnabled && m_xlxHostsFileUrl.isEmpty())//To avoid support nightmare, fill the url with the default one when xlx is enabled and the url is left empty
-		m_xlxHostsFileName = DEFAULT_XLX_HOSTS_FILE_URL;
+	if(m_xlxEnabled && m_xlxHostsFileUrl.IsEmpty())//To avoid support nightmare, fill the url with the default one when xlx is enabled and the url is left empty
+		m_xlxHostsFileUrl = DEFAULT_XLX_HOSTS_FILE_URL;
 
 	m_config->Read(m_name + KEY_STARNET_BAND1, &m_starNet1Band, DEFAULT_STARNET_BAND);
 
