7f71576b5ffe2425eabfa8138b8d8403c32f5164
Jonathan Naylor <naylorjs@yahoo.com>
2020-06-05 11:27:50 +0100

    Bump the version date.

--- Common/Version.h
+++ Common/Version.h
@@ -24,9 +24,9 @@
 const wxString VENDOR_NAME = wxT("G4KLX");
 
 #if defined(__WXDEBUG__)
-const wxString VERSION = wxT("20200604 - DEBUG");
+const wxString VERSION = wxT("20200605 - DEBUG");
 #else
-const wxString VERSION = wxT("20190604");
+const wxString VERSION = wxT("20190605");
 #endif
 
 #endif
